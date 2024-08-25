3ab67fc7074e56d3519b1fea79356ed3eb6d9eb4
Sedat Polat <sedatpolat355@gmail.com>
2024-08-25 10:09:17 +0300

    renderHourlyWeather() çalışıyor

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ getWeather(10,10,Intl.DateTimeFormat().resolvedOptions().timeZone)
 function renderWeather({current, daily, hourly}) {
   renderCurrentWeather(current)
   renderDailyWeather(daily)
-  // renderHourlyWeather(hourly)
+  renderHourlyWeather(hourly)
 
   document.body.classList.remove('blurred')
 }
@@ -60,16 +60,32 @@ function renderDailyWeather(daily) {
     const element = dayCardTemplate.content.cloneNode(true)
     setValue('temp', day.maxTemp, {parent: element})
     setValue('date', DAY_FORMATTER.format(day.timestamp), {parent: element})
- 
+    
     element.querySelector('[data-icon]').src = getIconUrl(day.iconCode)
     dailySection.append(element)
   })
-
 }
 
+//! Hourly weather --------------------------------------------------
+const HOUR_FORMATTER = new Intl.DateTimeFormat(undefined, {hour: 'numeric'})
+const hourlySection = document.querySelector("[data-hour-section]")
+const hourRowTemplate = document.getElementById('hour-row-template')
 
-
-
+function renderHourlyWeather(hourly) {
+  hourlySection.innerHTML = "";
+  hourly.forEach(hour => {
+    const element = hourRowTemplate.content.cloneNode(true)
+    setValue('temp', hour.temp, {parent: element})
+    setValue('fl-temp', hour.feelsLike, {parent: element})
+    setValue('wind', hour.windSpeed, {parent: element})
+    setValue('precip', hour.precip, {parent: element})
+    setValue('day', DAY_FORMATTER.format(hour.timestamp), {parent: element})
+    setValue('time', HOUR_FORMATTER.format(hour.timestamp), {parent: element})
+    
+    element.querySelector('[data-icon]').src = getIconUrl(hour.iconCode)
+    hourlySection.append(element)
+  })
+}
 
 
 
