9f1f34b26942c3e052a88fdf13d5290bfacc209b
Sedat Polat <sedatpolat355@gmail.com>
2024-08-25 08:07:37 +0300

    current weather getirildi

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 import "./style.css"
+import {getWeather} from "./weather";
+
+console.log(Intl.DateTimeFormat().resolvedOptions().timeZone)//Europe/Istanbul
+
+getWeather(10,10,Intl.DateTimeFormat().resolvedOptions().timeZone).then( (data) => {
+  console.log(data)
+})
